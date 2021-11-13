b222f7243b80b0384025b174ac42660f7d1cae2f
Dhruv1107 <dhruvsr4@gmail.com>
2021-11-14 02:01:00 +0530

    Made the function recursive

--- multiplycomplexnumbers.c
+++ multiplycomplexnumbers.c
@@ -9,8 +9,8 @@ void displayComplexNumbers(complexNum *complexNums,int index) {
 void *runner1(void *param) {
 	complexNum cn1 = *(complexNum *)param;
 	complexNum cn2 = *(complexNum *)(param + sizeof(complexNum));
-	printf("%d+i%d in thread\n",cn1.real,cn1.imaginary);
-	printf("%d+i%d in thread\n",cn2.real,cn2.imaginary);
+	//printf("%d+i%d in thread\n",cn1.real,cn1.imaginary);
+	//printf("%d+i%d in thread\n",cn2.real,cn2.imaginary);
 	complexNum *res = malloc(sizeof(complexNum));
 	complexNum ans;
 	ans.real = cn1.real * cn2.real - cn1.imaginary * cn2.imaginary;
@@ -19,32 +19,41 @@ void *runner1(void *param) {
 	return (void*) res;
 }
 
-int createThreads(complexNum *complexNums, int index) {
+complexNum createThreads(complexNum *complexNums, int index) {
 	//printf("Index:%d\n",index);
-	complexNum *res;
+	complexNum *res,*nextNums;
 	int noOfThreads = index/2;
 	pthread_t tid[noOfThreads];
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
-	printf("No of threads:%d\n",noOfThreads);
+	//printf("No of threads:%d\n",noOfThreads);
 	for(int i=0; i< noOfThreads; i++){
 		if(pthread_create(&tid[i],&attr,runner1,&complexNums[i*2]) != 0){
 			perror("Failed to create thread\n");
-			return 2;
 		}
 	}
-	for(int i=0; i< noOfThreads; i++){
+	if(noOfThreads == 1){
+		if(pthread_join(tid[0],(void**) &res) != 0){
+			perror("Failed to create thread\n");
+		}
+		return *res;	
+	}
+	int i;
+	nextNums = ( struct complex * ) malloc ((noOfThreads+1)* sizeof (struct complex));
+	for(i=0; i< noOfThreads; i++){
 		if(pthread_join(tid[i],(void**) &res) != 0){
 			perror("Failed to create thread\n");
-			return 3;
 		}
+		nextNums[i].real = res->real;
+		nextNums[i].imaginary = res->imaginary;
 		printf("Thread %d ans: %d+i%d\n",i,res->real,res->imaginary);
 	}
-	return 0;	
+	createThreads(nextNums,i);	
 }
 
 void multiplyComplexNumbers(complexNum *complexNums, int index) {
 	//printf("Index:%d\n",index);
 	//displayComplexNumbers(complexNums, index);
-	createThreads(complexNums, index);
+	complexNum ans = createThreads(complexNums, index);
+	printf("Ans:%d+i%d\n",ans.real,ans.imaginary);
 }
